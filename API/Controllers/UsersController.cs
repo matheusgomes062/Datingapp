408c0ae502a0d882f2d516a0558052ae5d995590
Matheus Gomes <matheusgomes062@gmail.com>
2020-11-23 17:53:01 -0300

    feature(api): continued UsersController.cs

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Controllers
@@ -6,6 +10,25 @@ namespace Controllers
     [Route("api/[controllers]")]
     public class UsersController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers(int id)
+        {
+            return _context.Users.Find(id);
+        }
     }
 }
